38685090bb69cce56fb160f44eeea5a44bb94a16
tradier <tradier@gmail.com>
2026-09-25 08:50:58 +0000

    updated public URL

--- apps/web/js/config.js
+++ apps/web/js/config.js
@@ -2,5 +2,5 @@
 // Change API_BASE_URL to point at your deployed API (e.g. Northflank).
 // For local development, use http://localhost:8000
 window.PURIFYDOCS_CONFIG = {
-  API_BASE_URL: "http://localhost:8000",
+  API_BASE_URL: "http://178.156.177.153:8000",
 };
